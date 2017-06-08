f6e08b92ef0b3c11358916f9ae63dbff21acc758
Aeone Singson <aeone.singson@gmail.com>
2017-06-08 08:18:35 -0700

    Add Lab 3: Incomplete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Aeone Singson - Code 201 - About Me
-// Lab 2 Resubmit
+// Lab 3
 
 'use strict';
 // design guessing game
@@ -7,7 +7,6 @@
 // var init
 var userPoints = 0;
 var answer;
-var question;
 
 // get user name
 var user = prompt('What is your name?');
@@ -15,86 +14,63 @@ alert('Hello, ' + user + '. Welcome!');
 alert('This is a guessing game to determine if you\'re my friend. You will answer five questions and if you answer enough of them correctly, I will share a secret with you. If not, I will feed you to a dragon.');
 alert('Let\'s begin!');
 
-// question 1
-question = 'Is my favorite color green? ';
-// auto lower-case at prompt for easy comparison
-answer = prompt(question).toLowerCase();
-// correct answer condition and response
-if (answer === 'y' || answer === 'yes') {
-  alert('Correct!');
-  // log points
-  userPoints++;
-  alert('You have ' + userPoints + ' total points.');
-// incorrect answer response
-} else {
-  alert('Wrong.');
-  alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
-}
-// log question and answer to console
-console.log(question + ': ' + answer);
+// array of questions to ask
+var questions = ['Is my favorite color green? '];
+questions.push('Am I tall? ');
+questions.push('Do I like bananas? ');
+questions.push('Do I drink coffee? ');
+questions.push('Do I like to draw? ');
+questions.push('Guess a number between 0-9: ');
+questions.push('What other cities have I lived in? ');
 
-// question 2
-question = 'Am I tall? ';
-answer = prompt(question).toLowerCase();
-if (answer === 'n' || answer === 'no') {
-  alert('Correct!');
-  userPoints++;
-  alert('You have ' + userPoints + ' total points.');
-} else {
-  alert('Wrong.');
-  alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
+// ask five questions
+for(var i = 0; i < questions.length; i++){
+  answer = prompt(questions[i]).toLowerCase();
+  if(i === 0 || i === 3 || i === 4){
+    if(answer === 'y' || answer === 'yes'){
+      alert('Correct!');
+      userPoints++;
+      alert('You have ' + userPoints + ' total points.');
+    } else {
+      alert('Wrong.');
+      alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
+    }
+  } else if (i === 1 || i === 2){
+    if(answer === 'n' || answer === 'no'){
+      alert('Correct!');
+      userPoints++;
+      alert('You have ' + userPoints + ' total points.');
+    } else {
+      alert('Wrong.');
+      alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
+    }
+  } else if (i === 5){
+    // guess a number
+    if(answer != 8){
+      for(var j = 0; j < 4; j++){
+        answer = prompt('Guess again: ');
+        if(answer == 8){
+          break;
+        }
+      }
+    } else {
+      alert('Correct!');
+      userPoints++;
+      alert('You have ' + userPoints + ' total points.');
+    }
+  } else if (i === 6){
+    // guess a city
+  }
 }
-console.log(question + ': ' + answer);
-
-// question 3
-question = 'Do I like bananas? ';
-answer = prompt(question).toLowerCase();
-if (answer === 'n' || answer === 'no') {
-  alert('Correct!');
-  userPoints++;
-  alert('You have ' + userPoints + ' total points.');
-} else {
-  alert('Wrong.');
-  alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
-}
-console.log(question + ': ' + answer);
-
-// question 4
-question = 'Do I drink coffee? ';
-answer = prompt(question).toLowerCase().toLowerCase();
-if (answer === 'y' || answer === 'yes') {
-  alert('Correct!');
-  userPoints++;
-  alert('You have ' + userPoints + ' total points.');
-} else {
-  alert('Wrong.');
-  alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
-}
-console.log(question + ': ' + answer);
-
-// question 5
-question = 'Do I like to draw? ';
-answer = prompt(question).toLowerCase();
-if (answer === 'y' || answer === 'yes') {
-  alert('Correct!');
-  userPoints++;
-  alert('You have ' + userPoints + ' total points.');
-} else {
-  alert('Wrong.');
-  alert('You have ' + userPoints + ' total points. The dragon is giddy with anticipation.');
-}
-console.log(question + ': ' + answer);
 
 // present score
 alert('Your total score is ' + userPoints + '.');
-// winning and losing
-if (userPoints >= 3) {
+if(userPoints >= 3){
   alert('Congratulations. You have earned a secret!');
   alert('Here is my secret...');
   alert('Once, I had a friend who I left in a room with a hungry dragon.');
-} else {
+}else{
   alert('Congratulations! You have lost!');
   alert('You have earned the wonderful honor of helping sustain a beautiful creature. Please wait here and my dragon will be with you shortly.');
 }
-
 alert('Goodbye!');
